2a45c4174cdba9d23d647a4ea7cc13caec6b7a5f
Siddi Nalgonda <srinivasnolgonda@gmail.com>
2024-01-04 09:44:34 +0530

    commit first

---
+++ gitHubproject/gitHubdemo/src/gitHubdemo/b4democlass.java
@@ -0,0 +1,11 @@
+package gitHubdemo;
+
+public class b4democlass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Github hello world");
+
+	}
+
+}
---
+++ gitHubproject/gitHubdemo/src/gitHubdemo/democlassB4.java
@@ -0,0 +1,10 @@
+package gitHubdemo;
+
+public class democlassB4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
